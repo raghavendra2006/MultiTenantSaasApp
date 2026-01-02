f45e6759d2dc8a70ac9259a0948aefa8a9ca1dfe
raghavendra2006 <patchipulusuraghavendra@gmail.com>
2026-01-02 23:08:34 +0530

    added to git

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
+
 const Navbar = () => {
   const { user, logout } = useAuth();
   const navigate = useNavigate();
